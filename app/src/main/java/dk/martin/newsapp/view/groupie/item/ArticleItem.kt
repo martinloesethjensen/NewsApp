301f14b838a6d58fcd815f067ee11ce68bbc3c1d
Martin L. Jensen <martinloesethjensen@gmail.com>
2019-04-28 20:36:50 +0200

    Fixed in-app web-view not being opened when user clicks on an article.

--- app/src/main/java/dk/martin/newsapp/view/groupie/item/ArticleItem.kt
+++ app/src/main/java/dk/martin/newsapp/view/groupie/item/ArticleItem.kt
@@ -1,16 +1,20 @@
 package dk.martin.newsapp.view.groupie.item
 
+import android.content.Intent
 import android.util.Log
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import dk.martin.newsapp.R
 import dk.martin.newsapp.model.Article
 import dk.martin.newsapp.service.module.GlideApp
+import dk.martin.newsapp.service.utils.ARTICLE_URL
+import dk.martin.newsapp.view.ui.WebViewActivity
 import kotlinx.android.synthetic.main.article_not_list.*
 
 class ArticleItem(private val article: Article) : Item() {
+
     override fun bind(viewHolder: ViewHolder, position: Int) {
-        Log.d("OnBindViewHolder", "${article.title}")
+        Log.d("ArticleItem", "Article title: ${article.title}")
 
         GlideApp.with(viewHolder.itemView)
             .load(article.urlToImage)
@@ -18,6 +22,17 @@ class ArticleItem(private val article: Article) : Item() {
         viewHolder.card_view_image_title.text = article.title
         viewHolder.card_view_image_description.text = article.description
         viewHolder.adapterPosition
+
+        viewHolder.itemView.setOnClickListener {
+            val intent = Intent(
+                it.context,
+                WebViewActivity::class.java
+            )
+
+            intent.putExtra(ARTICLE_URL, article.url)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            it.context.startActivity(intent)
+        }
     }
 
     override fun getLayout(): Int {
